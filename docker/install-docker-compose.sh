74bfacef02fefdb8e644b581118d963bd28dd255
root <root@cartobase-cat.cartobase.es>
2022-04-25 12:12:43 +0000

    Instalador docker compose

---
+++ docker/install-docker-compose.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# https://docs.docker.com/compose/install/
+set -e
+
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+curl \
+    -L https://raw.githubusercontent.com/docker/compose/1.29.2/contrib/completion/bash/docker-compose \
+    -o /etc/bash_completion.d/docker-compose
